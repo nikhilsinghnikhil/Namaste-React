3c325ec9df95d175908bdf7aa6d1ea808e9659de
NIKHIL <nikhilsinghnikhil121@gmail.com>
2025-09-10 19:38:55 +0530

    Your new changes message

---
+++ README.md
@@ -0,0 +1,18 @@
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimization 
+- Minification 
+- Bundling
+- Compressing
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
+- different dev and prod bundles
--- app.js
+++ app.js
@@ -1,33 +1,34 @@
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  [
-    React.createElement(
-      "div",
-      { id: "child" },
-      [
-        React.createElement("h1", {}, "I am an h1 tag"),
-        React.createElement("h2", {}, "I am an h2 tag"),
-      ]
-    ),
-    React.createElement(
-      "div",
-      { id: "child2" },
-      [
-        React.createElement("h1", {}, "I am an h1 tag"),
-        React.createElement("h2", {}, "I am an h2 tag"),
-      ]
-    )
-  ]
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+//React Element
 
 
 
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   [
+//     React.createElement(
+//       "div",
+//       { id: "child" },
+//       [
+//         React.createElement("h1", {}, "This is namaste-React 🚀"),
+//         React.createElement("h2", {}, "I am an h2 tag"),
+//       ]
+//     ),
+//     React.createElement(
+//       "div",
+//       { id: "child2" },
+//       [
+//         React.createElement("h1", {}, "I am an h1 tag"),
+//         React.createElement("h2", {}, "I am an h2 tag"),
+//       ]
+//     )
+//   ]
+// );
+//         console.log(parent);  // it gives object.
+//         const root = ReactDOM.createRoot(document.getElementById("root"));
+//         root.render(parent);
 
-
-const heading = React.createElement("h1" , {id:"heading"} , "Hello World form React");
-        const root = ReactDOM.createRoot(document.getElementById("root"));
-        root.render(parent);
-
-        console.log(parent);  // it gives object.
 
